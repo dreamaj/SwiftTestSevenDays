753678ba61afa3ebcf3c66ef6683c3acc3c5f64a
优聚投 <youjutou@youjutou.local>
2016-04-13 09:24:42 +0800

    Initial Commit

---
+++ SwiftTestSevenDays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTestSevenDays
+//
+//  Created by 优聚投 on 16/4/13.
+//  Copyright © 2016年 Dreamaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
